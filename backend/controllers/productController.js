ed326720f447600c3eb8ba6bddddb9c1c1649cf4
malav22 <malavakhani58@gmail.com>
2022-01-16 00:01:42 +0530

    user / review api completed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -85,3 +85,46 @@ exports.deleteProduct = catchAsyncErrors(async(req, res, next) =>{
         message : "Product deleted successfully",
     });
 });
+
+//Get all reviews of a product
+exports.getProductReviews = catchAsyncErrors(async(req,res,next) =>{
+    const product = await Product.findById(req.query.id);
+
+    if(!product){
+        return next(new ErrorHander("Product not found",404));
+    }
+    res.status(200).json({
+        success : true,
+        reviews : product.reviews,
+    });
+});
+
+//Delete a reviews
+exports.deleteReview = catchAsyncErrors(async(req, res,next) =>{
+    const product = await Product.findById(req.query.productId);
+
+    if(!product){
+        return next(new ErrorHander("Product not found",404));
+    }
+
+    const reviews = product.reviews.filter( rev => rev._id.toString() !== req.query.id.toString());
+    
+    let avg = 0;
+    reviews.forEach(rev => {
+        avg += rev.rating;
+    });
+    const rating = avg / reviews.length;
+    const numOfReviews = reviews.length;
+
+    await Product.findByIdAndUpdate(req.query.productId,{reviews,rating,numOfReviews},{
+        new:true,
+        runValidators : true,
+        useFindAndModify : false
+    });
+
+
+    res.status(200).json({
+        success : true,
+        message : "review deleted",
+    });
+});
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllProducts, createProduct , updateProduct, deleteProduct, getProductDetails } = require('../controllers/productController');
+const { getAllProducts, createProduct , updateProduct, deleteProduct, getProductDetails, getProductReviews, deleteReview } = require('../controllers/productController');
 const { createProductReview } = require('../controllers/userController');
 const { isAuthenticatedUser,authorizeRoles } = require('../middleware/auth');
 
@@ -14,5 +14,8 @@ router.route('/admin/product/:id').put(isAuthenticatedUser,authorizeRoles("admin
 router.route('/product/:id').get(getProductDetails);
 
 router.route('/review').put(isAuthenticatedUser,createProductReview);
+router.route('/reviews').get(getProductReviews).delete(isAuthenticatedUser,deleteReview);
+
+router.route('/review/delete').put(isAuthenticatedUser,createProductReview);
 
 module.exports = router;
