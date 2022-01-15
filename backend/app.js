80cf0251d5cdb3367c6cf84805807bf1794c559b
malav22 <malavakhani58@gmail.com>
2022-01-16 02:00:06 +0530

    Order Api

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,12 @@ app.use(cookieParser());
 //Routes import
 const product = require('./routes/productRoute');
 const user = require('./routes/userRoute');
+const order = require('./routes/orderRoute');
+
 app.use("/api/v1",product);
 app.use("/api/v1",user);
+app.use("/api/v1",order);
+
 
 //Middleware for error handling
 app.use(errorMiddleware);
