82f72fcb0ba07b6fb5fcc971df9cba8a9a956bf0
malav22 <malavakhani58@gmail.com>
2022-01-18 15:02:41 +0530

    Frontend , Header and Footer

---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component,Fragment } from 'react';
+
+const Home = () =>{
+
+    return (<>{
+        console.log("hasdf")
+    }
+        <h4>Hey there</h4>
+    </>)
+}
+
+export default Home;
